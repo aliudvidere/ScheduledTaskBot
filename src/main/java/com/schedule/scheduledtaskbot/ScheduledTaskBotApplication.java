6ef6df7acd207398fd1fcc5f97bb143d5fd101c0
Vladimir Berman <vberman@diasoft.ru>
2025-03-03 16:24:40 +0300

    Initial commit

---
+++ src/main/java/com/schedule/scheduledtaskbot/ScheduledTaskBotApplication.java
@@ -0,0 +1,13 @@
+package com.schedule.scheduledtaskbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledTaskBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduledTaskBotApplication.class, args);
+    }
+
+}
